d8d4efad150f222d0d2083317043c899b6349a4b
Srinivas Kommu <srini@github.com>
2016-01-20 21:07:16 -0800

    test

---
+++ src/main/java/com/kata/apps/gildedrose/App.java
@@ -0,0 +1,13 @@
+package com.kata.apps.gildedrose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
