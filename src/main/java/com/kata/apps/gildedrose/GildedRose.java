c65f44d23c5f0caed4648f3b7c953f2ae339e301
Srinivas Kommu <srini@github.com>
2016-01-20 22:57:38 -0800

    update

--- src/main/java/com/kata/apps/gildedrose/GildedRose.java
+++ src/main/java/com/kata/apps/gildedrose/GildedRose.java
@@ -23,12 +23,7 @@ public class GildedRose {
 	
 	public static void main(String[] args) {
 		items = new ArrayList<ItemWrapper>();
-		items.add(new ItemWrapper("+5 Dexterity Vest", 10, 20));
-		items.add(new ItemWrapper("Aged Brie", 2, 0));
-		items.add(new ItemWrapper("Elixir of the Mongoose", 5, 7));
-		items.add(new ItemWrapper("Sulfuras, Hand of Ragnaros", 0, 80));
-		items.add(new ItemWrapper("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-		items.add(new ItemWrapper("Conjured Mana Cake", 3, 6));
+
 
 		updateQuality();
 	}
